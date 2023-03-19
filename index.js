66d1ac9fc983be8cf17e74cf324fbe7170bd7ced
Sukhdeep Singh <pannusukhu@gmail.com>
2023-03-19 23:57:14 +0530

    adding node port to process'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 80;
+const port = process.env.PORT || 3001;
 const apiRouter = require('./routes/api');
 const path = require('path');
 const fs = require('fs/promises');
